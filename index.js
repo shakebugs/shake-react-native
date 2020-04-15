0d73a7aa38e0249914e26525b89b5cf023bf3813
mstanic <mislav.stanic@hotmail.com>
2020-04-15 14:56:12 +0200

    ShakeInvocaionEvent constants added

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import { NativeModules } from 'react-native';
+import {NativeModules} from 'react-native';
 
-const { Shake } = NativeModules;
+const {Shake} = NativeModules;
+
+export const ShakeInvocationEvent = {
+    BUTTON: 'BUTTON',
+    SHAKE: 'SHAKE',
+    SCREENSHOT: 'SCREENSHOT',
+};
 
 export default Shake;
