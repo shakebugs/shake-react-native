f4a2ac2fd810493074325d2febc7e175c9270379
mstanic <mislav.stanic@hotmail.com>
2020-04-16 18:08:41 +0200

    Updated scripts

--- scripts/environment.bat
+++ scripts/environment.bat
@@ -1,2 +1,3 @@
+powershell -Command "(gc package.json) -replace 'MODULE_NAME', "$env:MODULE_NAME" | Out-File -encoding ASCII package.json"
 powershell -Command "(gc android/build.gradle) -replace 'ANDROID_DEPENDENCY', "$env:ANDROID_DEPENDENCY" | Out-File -encoding ASCII android/build.gradle"
 powershell -Command "(gc react-native-shake.podspec) -replace 'IOS_DEPENDENCY', "$env:IOS_DEPENDENCY" | Out-File -encoding ASCII react-native-shake.podspec"
