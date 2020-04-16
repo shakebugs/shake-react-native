d4db0f83b09ee52e5e262a1b56f0a005377eb8cc
mstanic <mislav.stanic@hotmail.com>
2020-04-16 18:37:04 +0200

    Update package-lock.json when version is updated

--- scripts/environment.bat
+++ scripts/environment.bat
@@ -1,3 +1,4 @@
 powershell -Command "(gc package.json) -replace 'MODULE_NAME', "$env:MODULE_NAME" | Out-File -encoding ASCII package.json"
 powershell -Command "(gc android/build.gradle) -replace 'ANDROID_DEPENDENCY', "$env:ANDROID_DEPENDENCY" | Out-File -encoding ASCII android/build.gradle"
 powershell -Command "(gc react-native-shake.podspec) -replace 'IOS_DEPENDENCY', "$env:IOS_DEPENDENCY" | Out-File -encoding ASCII react-native-shake.podspec"
+npm i --package-lock-only
--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,3 +1,4 @@
 sed "s|MODULE_NAME|$MODULE_NAME|g" package.json > changed.txt && mv changed.txt package.json
 sed "s|ANDROID_DEPENDENCY|$ANDROID_DEPENDENCY|g" android/build.gradle > changed.txt && mv changed.txt android/build.gradle
 sed "s|IOS_DEPENDENCY|$IOS_DEPENDENCY|g" react-native-shake.podspec > changed.txt && mv changed.txt react-native-shake.podspec
+npm i --package-lock-only
