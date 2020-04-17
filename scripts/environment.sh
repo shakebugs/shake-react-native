2de2519d62694ee9ed434b5cf61e4a77e360569a
Mislav Stanić <mislav.stanic@decode.agency>
2020-04-17 16:41:42 +0200

    Local environments initial setup

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,4 +1,11 @@
-sed "s|MODULE_NAME|$MODULE_NAME|g" package.json > changed.txt && mv changed.txt package.json
-sed "s|ANDROID_DEPENDENCY|$ANDROID_DEPENDENCY|g" android/build.gradle > changed.txt && mv changed.txt android/build.gradle
-sed "s|IOS_DEPENDENCY|$IOS_DEPENDENCY|g" react-native-shake.podspec > changed.txt && mv changed.txt react-native-shake.podspec
+#!/bin/bash
+# Install npe
+npm install npe --global
+
+# Change package.json name
+npe name $MODULE_NAME
 npm i --package-lock-only
+
+# Update dependencies
+sed "s|$System.env.ANDROID_DEPENDENCY|$ANDROID_DEPENDENCY|g" android/build.gradle > changed.txt && mv changed.txt android/build.gradle
+sed "s|#{ENV['IOS_DEPENDENCY']}|$IOS_DEPENDENCY|g" react-native-shake.podspec > changed.txt && mv changed.txt react-native-shake.podspec
