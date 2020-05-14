02daeec4979de1e52ea7ec0b74ddfc2e11db7a00
Mislav Stanić <mislav.stanic@decode.agency>
2020-05-14 13:19:37 +0200

    Removed spaces

--- scripts/run_script.js
+++ scripts/run_script.js
@@ -5,10 +5,10 @@ const glob = require('glob');
 glob('node_modules/@shakebugs/react-native-shake?(-uat|-staging)/scripts', {}, function(error, match) {
     const filePath = match.toString();
     try {
-        execute (`node ${filePath}/add_maven_link.js`);
-        execute (`node ${filePath}/add_dependency_shake.js`);
-        execute (`node ${filePath}/add_shake_init_code.js`);
-        execute (`node ${filePath}/add_touch_tracking.js`);
+        execute(`node ${filePath}/add_maven_link.js`);
+        execute(`node ${filePath}/add_dependency_shake.js`);
+        execute(`node ${filePath}/add_shake_init_code.js`);
+        execute(`node ${filePath}/add_touch_tracking.js`);
     } catch(error) {
         console.error(error);
         process.exit(-1);
