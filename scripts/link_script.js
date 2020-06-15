404f8579afbe96594d658a5d70a664849ac8fff2
Mislav Stanić <mislav.stanic@decode.agency>
2020-06-15 15:39:18 +0200

    Update Shake version to 10

--- scripts/link_script.js
+++ scripts/link_script.js
@@ -6,19 +6,19 @@ const pathUat = "node_modules/@shakebugs/react-native-shake-uat/scripts";
 const pathStg = "node_modules/@shakebugs/react-native-shake-staging/scripts";
 
 if (fs.existsSync(pathPrd)) {
-    execute(`node ${pathPrd}/add_gradle_dependency.js "com.shakebugs.android:shake:9.0.+"`);
+    execute(`node ${pathPrd}/add_gradle_dependency.js "com.shakebugs.android:shake:10.0.+"`);
     execute(`node ${pathPrd}/add_maven_repository.js`);
     execute(`node ${pathPrd}/add_multidex_enabled.js`);
     execute(`node ${pathPrd}/add_shake_init_code.js`);
     execute(`node ${pathPrd}/add_touch_tracking.js`);
 } else if (fs.existsSync(pathUat)) {
-    execute(`node ${pathUat}/add_gradle_dependency.js "com.shakebugs.android:shake-uat:9.0.+"`);
+    execute(`node ${pathUat}/add_gradle_dependency.js "com.shakebugs.android:shake-uat:10.0.+"`);
     execute(`node ${pathUat}/add_maven_repository.js`);
     execute(`node ${pathUat}/add_multidex_enabled.js`);
     execute(`node ${pathUat}/add_shake_init_code.js`);
     execute(`node ${pathUat}/add_touch_tracking.js`);
 } else if (fs.existsSync(pathStg)) {
-    execute(`node ${pathStg}/add_gradle_dependency.js "com.shakebugs.android:shake-staging:9.0.+"`);
+    execute(`node ${pathStg}/add_gradle_dependency.js "com.shakebugs.android:shake-staging:10.0.+"`);
     execute(`node ${pathStg}/add_maven_repository.js`);
     execute(`node ${pathStg}/add_multidex_enabled.js`);
     execute(`node ${pathStg}/add_shake_init_code.js`);
