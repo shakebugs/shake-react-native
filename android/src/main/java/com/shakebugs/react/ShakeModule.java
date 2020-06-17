5fd19f7005a3ea2b3cb606706258dc2c476c6922
Mislav Stanić <mislav.stanic@decode.agency>
2020-06-17 16:30:01 +0200

    Fix for screenshot permissions requesting

--- android/src/main/java/com/shakebugs/react/ShakeModule.java
+++ android/src/main/java/com/shakebugs/react/ShakeModule.java
@@ -10,6 +10,7 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.shakebugs.react.db.SqliteDatabase;
 import com.shakebugs.react.utils.Mapper;
+import com.shakebugs.react.utils.Permissions;
 import com.shakebugs.shake.Shake;
 import com.shakebugs.shake.ShakeInvocationEvent;
 import com.shakebugs.shake.internal.data.NetworkRequest;
@@ -17,6 +18,9 @@ import com.shakebugs.shake.report.ShakeFile;
 import com.shakebugs.shake.report.ShakeReportData;
 
 import java.util.List;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class ShakeModule extends ReactContextBaseJavaModule {
     private final Application application;
@@ -58,8 +62,25 @@ public class ShakeModule extends ReactContextBaseJavaModule {
             public void run() {
                 ShakeInvocationEvent[] invocationEvents = Mapper.mapToShakeInvocationEvents(stringList);
                 Shake.setInvocationEvents(invocationEvents);
+
+                checkScreenshotPermissions(invocationEvents);
             }
         });
+    }
+
+    /**
+     * This is introduced as fix because SDK 9.0.3 requests permissions just on activity start
+     *
+     * @param invocationEvents invocation events to set
+     */
+    private void checkScreenshotPermissions(ShakeInvocationEvent[] invocationEvents) {
+        for (ShakeInvocationEvent event: invocationEvents) {
+            if (event.equals(ShakeInvocationEvent.SCREENSHOT)) {
+                Permissions.requestPermission(getCurrentActivity(), READ_EXTERNAL_STORAGE);
+                Permissions.requestPermission(getCurrentActivity(), WRITE_EXTERNAL_STORAGE);
+                return;
+            }
+        }
     }
 
     @ReactMethod
