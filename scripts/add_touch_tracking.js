f5ae4d3de0e7743a63be020c90acd79e243821ca
Mislav Stanić <mislav.stanic@decode.agency>
2020-07-15 14:52:53 +0200

    Update autolinking scripts

--- scripts/add_touch_tracking.js
+++ scripts/add_touch_tracking.js
@@ -1,24 +1,20 @@
 const fs = require('fs');
 const glob = require('glob');
 
-const shakeImport = ['import com.shakebugs.shake.Shake;', 'import android.view.MotionEvent;'];
-const touchHandle = [
-    '\t@Override',
-    '\tpublic boolean dispatchTouchEvent(MotionEvent ev) {',
-    '\t\tShake.handleTouchEvent(ev, this);',
-    '\t\treturn super.dispatchTouchEvent(ev);',
-    '\t}',
-];
-const mainActivity = 'public class MainActivity extends ReactActivity {';
+const shakeImport =
+`
+import com.shakebugs.shake.Shake;
+import android.view.MotionEvent;
 
-const renderMutlilineString = function (stringArray) {
-    let result = '';
-    for (i in stringArray) {
-        result += stringArray[i] + '\n';
-    }
-    result += '\n';
-    return result;
-}
+`;
+const touchHandle =
+`
+  @Override
+  public boolean dispatchTouchEvent(MotionEvent ev) {
+      Shake.handleTouchEvent(ev, this);
+      return super.dispatchTouchEvent(ev);
+  }`;
+const mainActivity = 'public class MainActivity extends ReactActivity {';
 
 glob('android/app/src/main/java/**/MainActivity.java', {}, function (error, match) {
     const filePath = match.toString();
@@ -26,13 +22,10 @@ glob('android/app/src/main/java/**/MainActivity.java', {}, function (error, matc
     let fileContents = fs.readFileSync(filePath, 'utf8');
     if (!fileContents.includes('Shake.handleTouchEvent')) {
         try {
-            let importsContents = renderMutlilineString(shakeImport);
-            let touchHandleContents = renderMutlilineString(touchHandle);
-
             let mainActivityIndex = fileContents.indexOf(mainActivity);
             let lastBraceIndex = fileContents.lastIndexOf('}');
 
-            let contents = fileContents.substring(0, mainActivityIndex) + importsContents + fileContents.substring(mainActivityIndex, lastBraceIndex) + touchHandleContents + '}';
+            let contents = fileContents.substring(0, mainActivityIndex) + shakeImport + fileContents.substring(mainActivityIndex, lastBraceIndex) + touchHandle + '\n}';
 
             fs.writeFileSync(filePath, contents)
         } catch (error) {
--- scripts/remove_touch_tracking.js
+++ scripts/remove_touch_tracking.js
@@ -2,12 +2,14 @@ const glob = require('glob');
 const utils = require('./utils.js');
 
 const shakeImport = 'import com.shakebugs.shake.Shake;';
+const eventImport = 'import android.view.MotionEvent;';
 const touchHandle = 'Shake.handleTouchEvent(ev, this);';
 
 glob('android/app/src/main/java/**/MainActivity.java', {}, function (error, match) {
     const filePath = match.toString();
     try {
         utils.removeLine(filePath, shakeImport);
+        utils.removeLine(filePath, eventImport);
         utils.removeLine(filePath, touchHandle);
     } catch (error) {
         console.error(error);
