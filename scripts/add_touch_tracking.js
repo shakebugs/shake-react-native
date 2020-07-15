c3dc7396f0ca1a14f71192c301e457791af4f1e1
Mislav Stanić <mislav.stanic@decode.agency>
2020-07-15 11:59:25 +0200

    Update autolinking

--- scripts/add_touch_tracking.js
+++ scripts/add_touch_tracking.js
@@ -1,45 +1,41 @@
 const fs = require('fs');
 const glob = require('glob');
-const importsStr = ['import android.os.Bundle;', 'import com.shakebugs.react.TouchTracker;', 'import android.view.MotionEvent;'];
-const touchTrackerVar = '\n  private TouchTracker touchTracker;\n\n';
-const onCreateStr = [
-    '  @Override',
-    '  protected void onCreate(Bundle savedInstanceState) {',
-    '\tsuper.onCreate(savedInstanceState);',
-    '\ttouchTracker = new TouchTracker(getApplicationContext());',
-    '  }',
-];
-const handleEventStr = [
-    '  @Override',
-    '  public boolean dispatchTouchEvent(MotionEvent ev) {',
-    '\ttouchTracker.handleTouchEvent(ev, this);',
-    '\treturn super.dispatchTouchEvent(ev);',
-    '  }',
+
+const shakeImport = ['import com.shakebugs.shake.Shake;', 'import android.view.MotionEvent;'];
+const touchHandle = [
+    '\t@Override',
+    '\tpublic boolean dispatchTouchEvent(MotionEvent ev) {',
+    '\t\tShake.handleTouchEvent(ev, this);',
+    '\t\treturn super.dispatchTouchEvent(ev);',
+    '\t}',
 ];
 const mainActivity = 'public class MainActivity extends ReactActivity {';
 
-const renderMutlilineString = function(stringArray) {
+const renderMutlilineString = function (stringArray) {
     let result = '';
-    for(i in stringArray) {
+    for (i in stringArray) {
         result += stringArray[i] + '\n';
     }
     result += '\n';
     return result;
 }
 
-glob('android/app/src/main/java/**/MainActivity.java', {}, function(error, match) {
+glob('android/app/src/main/java/**/MainActivity.java', {}, function (error, match) {
     const filePath = match.toString();
-    let fileContents = fs.readFileSync(`${filePath}`, 'utf8');
-    if(!fileContents.includes('TouchTracker')) {
+
+    let fileContents = fs.readFileSync(filePath, 'utf8');
+    if (!fileContents.includes('Shake.handleTouchEvent')) {
         try {
+            let importsContents = renderMutlilineString(shakeImport);
+            let touchHandleContents = renderMutlilineString(touchHandle);
+
             let mainActivityIndex = fileContents.indexOf(mainActivity);
-            let importsContents = renderMutlilineString(importsStr);
-            let onCreateContents = renderMutlilineString(onCreateStr);
-            let handleEventContents = renderMutlilineString(handleEventStr);
-            let lastBrace = fileContents.lastIndexOf('}');
-            let contents = fileContents.substring(0, mainActivityIndex) + importsContents + fileContents.substring(mainActivityIndex, lastBrace) + touchTrackerVar + onCreateContents + handleEventContents + '}';
+            let lastBraceIndex = fileContents.lastIndexOf('}');
+
+            let contents = fileContents.substring(0, mainActivityIndex) + importsContents + fileContents.substring(mainActivityIndex, lastBraceIndex) + touchHandleContents + '}';
+
             fs.writeFileSync(filePath, contents)
-        } catch(error) {
+        } catch (error) {
             console.error(error);
             process.exit(-1);
         }
