617a5f2e2fe15c7cc92f2a40e4b5e33d4bef2069
Mislav Stanić <mislav.stanic@decode.agency>
2020-08-24 13:02:22 +0200

    Fix typescript declarations

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,15 @@
-export namespace Shake {
-    function start(): void;
-    function show(): void;
-    function setEnabled(enabled: boolean): void;
-    function setEnableActivityHistory(enableActivityHistory: boolean): void;
-    function setEnableBlackBox(enableBlackBox: boolean): void;
-    function setEnableInspectScreen(enableInspectScreen: boolean): void;
-    function setShowFloatingReportButton(showFloatingReportButton: boolean): void;
-    function setInvokeShakeOnShaking(invokeOnShake: boolean): void;
-    function setInvokeShakeOnScreenshot(invokeOnScreenshot: boolean): void;
-    function setShakeReportData(files: Array<string>, quickFacts: string): void;
-    function silentReport(description: string, filesArray: Array<string>,
-                  quicFacts:string, configurationMap: object): void;
-    function insertNetworkRequest(request: object): void;
+declare module '@shakebugs/react-native-shake' {
+    export function start(): void;
+    export function show(): void;
+    export function setEnabled(enabled: boolean): void;
+    export function setEnableActivityHistory(enableActivityHistory: boolean): void;
+    export function setEnableBlackBox(enableBlackBox: boolean): void;
+    export function setEnableInspectScreen(enableInspectScreen: boolean): void;
+    export function setShowFloatingReportButton(showFloatingReportButton: boolean): void;
+    export function setInvokeShakeOnShaking(invokeOnShake: boolean): void;
+    export function setInvokeShakeOnScreenshot(invokeOnScreenshot: boolean): void;
+    export function setShakeReportData(files: Array<string>, quickFacts: string): void;
+    export function silentReport(description: string, filesArray: Array<string>,
+                                 quicFacts:string, configurationMap: object): void;
+    export function insertNetworkRequest(request: object): void;
 }
