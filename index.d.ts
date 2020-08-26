bc570005e284d87ad85cdbee4b740ec41dfe9271
Mislav Stanić <mislav.stanic@decode.agency>
2020-08-26 16:08:17 +0200

    Update typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,63 @@
 declare module '@shakebugs/react-native-shake' {
     export function start(): void;
+
     export function show(): void;
+
     export function setEnabled(enabled: boolean): void;
+
     export function setEnableActivityHistory(enableActivityHistory: boolean): void;
+
     export function isEnableActivityHistory(): Promise<boolean>;
+
     export function setEnableBlackBox(enableBlackBox: boolean): void;
+
     export function isEnableBlackBox(): Promise<boolean>;
+
     export function setEnableInspectScreen(enableInspectScreen: boolean): void;
+
     export function isEnableInspectScreen(): Promise<boolean>;
+
     export function setShowFloatingReportButton(showFloatingReportButton: boolean): void;
+
     export function isShowFloatingReportButton(): Promise<boolean>;
+
     export function setInvokeShakeOnShaking(invokeOnShake: boolean): void;
+
     export function isInvokeShakeOnShaking(): Promise<boolean>;
+
     export function setInvokeShakeOnScreenshot(invokeOnScreenshot: boolean): void;
+
     export function isInvokeShakeOnScreenshot(): Promise<boolean>;
-    export function setShakeReportData(files: Array<string>, quickFacts: string): void;
-    export function silentReport(description: string, filesArray: Array<string>,
-                                 quickFacts:string, configurationMap: object): void;
+
+    export function setShakeReportData(files: Array<ShakeFile>,
+                                       quickFacts: string): void;
+
+    export function silentReport(description: string,
+                                 files: Array<ShakeFile>,
+                                 quickFacts: string,
+                                 configuration: ShakeReportConfiguration): void;
+
     export function insertNetworkRequest(request: object): void;
+
+    export class ShakeReportConfiguration {
+        blackBoxData: boolean;
+        activityHistoryData: boolean;
+        screenshot: boolean;
+        showReportSentMessage: boolean;
+    }
+
+    export class ShakeFile {
+        name: string;
+        path: string;
+    }
+
+    export namespace ShakeFile {
+        function create(filePath: string, fileName?: string): ShakeFile
+    }
+
+    export namespace NetworkTracker {
+        function isEnabled(): boolean
+
+        function setEnabled(enabled: boolean): void
+    }
 }
