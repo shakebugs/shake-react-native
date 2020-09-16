f240cf8fbcd5f277383a2fec53036504b8870b52
Mislav Stanić <mislav.stanic@decode.agency>
2020-09-16 14:44:06 +0200

    Handle file extension on attach

--- src/models/ShakeFile.js
+++ src/models/ShakeFile.js
@@ -3,14 +3,17 @@ class ShakeFile {
     path;
 
     static create(filePath, fileName = "") {
+        const fileDetails = this._parseFilePath(filePath);
         if (!fileName) {
-            fileName = this._parseFilePath(filePath).name;
+            fileName = fileDetails.name + fileDetails.ext;
+        } else {
+            fileName = fileName + fileDetails.ext;
         }
 
         let shakeFile = new ShakeFile();
         shakeFile.path = filePath;
         shakeFile.name = fileName;
-        
+
         return shakeFile;
     }
 
