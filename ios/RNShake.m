872a2e300627f1a2fde2c64de779d443b3fc38df
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2020-10-29 11:21:44 +0000

    10.0.4 [skip ci]

--- ios/RNShake.m
+++ ios/RNShake.m
@@ -6,7 +6,7 @@
     if(self == [RNShake class]) {
         NSDictionary *platformAndSdkVersionDict = @{
             @"platform": @"ReactNative",
-            @"sdkVersion": @"10.0.3"
+            @"sdkVersion": @"10.0.4"
         };
         NSNumber *disableDueToRN = @YES;
         [SHKShake performSelector:sel_getUid(@"_setNetworkRequestReporterDisabledDueToRN:".UTF8String) withObject:disableDueToRN];
