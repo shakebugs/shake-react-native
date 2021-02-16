41b4beaea75ae9cff0b3306d5cc61789e379229f
Mislav Stanić <mislav.stanic@decode.agency>
2021-02-16 17:43:09 +0100

    SHK-3634: Update auto linking scripts

--- react-native.config.js
+++ react-native.config.js
@@ -1,42 +1,36 @@
 module.exports = {
     commands: [
         {
-            name: 'add-shake',
+            name: "add-shake",
             func: () => {
-                const process = require('child_process');
+                const process = require("child_process");
                 const fs = require("fs");
 
                 const pathPrd = "node_modules/@shakebugs/react-native-shake/scripts/link_script.js";
-                const pathUat = "node_modules/@shakebugs/react-native-shake-uat/scripts/link_script.js";
                 const pathStg = "node_modules/@shakebugs/react-native-shake-staging/scripts/link_script.js";
 
                 if (fs.existsSync(pathPrd)) {
                     process.exec(`node ${pathPrd}`);
-                } else if (fs.existsSync(pathUat)) {
-                    process.exec(`node ${pathUat} `);
                 } else if (fs.existsSync(pathStg)) {
                     process.exec(`node ${pathStg}`);
                 }
-            }
+            },
         },
         {
-            name: 'remove-shake',
+            name: "remove-shake",
             func: () => {
-                const process = require('child_process');
+                const process = require("child_process");
                 const fs = require("fs");
 
                 const pathPrd = "node_modules/@shakebugs/react-native-shake/scripts/unlink_script.js";
-                const pathUat = "node_modules/@shakebugs/react-native-shake-uat/scripts/unlink_script.js";
                 const pathStg = "node_modules/@shakebugs/react-native-shake-staging/scripts/unlink_script.js";
 
                 if (fs.existsSync(pathPrd)) {
                     process.exec(`node ${pathPrd}`);
-                } else if (fs.existsSync(pathUat)) {
-                    process.exec(`node ${pathUat} `);
                 } else if (fs.existsSync(pathStg)) {
                     process.exec(`node ${pathStg}`);
                 }
-            }
-        }
+            },
+        },
     ],
 };
--- scripts/add_touch_tracking.js
+++ scripts/add_touch_tracking.js
@@ -1,36 +1,23 @@
-const fs = require('fs');
-const glob = require('glob');
+const glob = require("glob");
+const utils = require("./utils.js");
 
-const shakeImport =
-`
-import com.shakebugs.shake.Shake;
-import android.view.MotionEvent;
+const shakeImport = "import com.shakebugs.shake.Shake;";
+const eventImport = "import android.view.MotionEvent;";
+const shakeHandle = "Shake.handleTouchEvent(ev, this);";
+const dispatchMethod = `
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Shake.handleTouchEvent(ev, this);
+        return super.dispatchTouchEvent(ev);
+    }`;
 
-`;
-const touchHandle =
-`
-  @Override
-  public boolean dispatchTouchEvent(MotionEvent ev) {
-      Shake.handleTouchEvent(ev, this);
-      return super.dispatchTouchEvent(ev);
-  }`;
-const mainActivity = 'public class MainActivity extends ReactActivity {';
-
-glob('android/app/src/main/java/**/MainActivity.java', {}, function (error, match) {
+glob("android/app/src/main/java/**/MainActivity.java", {}, function (error, match) {
     const filePath = match.toString();
 
-    let fileContents = fs.readFileSync(filePath, 'utf8');
-    if (!fileContents.includes('Shake.handleTouchEvent')) {
-        try {
-            let mainActivityIndex = fileContents.indexOf(mainActivity);
-            let lastBraceIndex = fileContents.lastIndexOf('}');
-
-            let contents = fileContents.substring(0, mainActivityIndex) + shakeImport + fileContents.substring(mainActivityIndex, lastBraceIndex) + touchHandle + '\n}';
-
-            fs.writeFileSync(filePath, contents)
-        } catch (error) {
-            console.error(error);
-            process.exit(-1);
-        }
+    if (!utils.containsLine(filePath, shakeHandle)) {
+        utils.addLineBefore(filePath, "class MainActivity", eventImport);
+        utils.addLineBefore(filePath, "class MainActivity", shakeImport);
+        utils.addLineBefore(filePath, "class MainActivity", "");
+        utils.addLineAfter(filePath, "class MainActivity", dispatchMethod);
     }
 });
