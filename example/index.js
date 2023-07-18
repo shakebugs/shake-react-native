b0c454a9e5db7f396bc4e44ed2b11057abf7fb0a
Mislav Stanić <mislav.stanic@shakebugs.com>
2023-07-18 14:51:52 +0200

    SHK-6622: Shake push notifications for iOS

--- example/index.js
+++ example/index.js
@@ -1,8 +1,10 @@
-import {AppRegistry, PermissionsAndroid} from 'react-native';
+import {AppRegistry, PermissionsAndroid, Platform} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import Shake from 'react-native-shake';
+
 import messaging from '@react-native-firebase/messaging';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 const initShake = () => {
   const CLIENT_ID = 'HtTFUmUziF5Qjk1XLraAJXtVB1cL62yHWWqsDnrG';
@@ -23,28 +25,38 @@ const initShake = () => {
   Shake.registerUser('pero');
 };
 
-const setupPushNotifications = async () => {
-  // Notifications permission required for Android 33+
-  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
-
-  // Firebase push notifications background handler
-  messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Message handled in the background!', remoteMessage);
-    Shake.showChatNotification(remoteMessage.data);
-  });
-  // Firebase push notifications foreground handler
-  messaging().onMessage(async remoteMessage => {
-    console.log('Message handled in the background!', remoteMessage);
-    Shake.showChatNotification(remoteMessage.data);
-  });
-
-  // Get FCM token for current session
-  const fcmToken = await messaging().getToken();
-  console.log(fcmToken);
-  Shake.setPushNotificationsToken(fcmToken);
+const requestNotificationPermission = () => {
+  if (Platform.OS === 'android') {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+  } else {
+    PushNotificationIOS.requestPermissions();
+  }
 };
 
+const setupFirebaseNotifications = async () => {
+  if (Platform.OS === 'android') {
+    // Firebase push notifications background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+      Shake.showChatNotification(remoteMessage.data);
+    });
+    // Firebase push notifications foreground handler
+    messaging().onMessage(async remoteMessage => {
+      console.log('Message handled in the foreground!', remoteMessage);
+      Shake.showChatNotification(remoteMessage.data);
+    });
+
+    // Get FCM token for current session
+    const fcmToken = await messaging().getToken();
+    console.log(fcmToken);
+    Shake.setPushNotificationsToken(fcmToken);
+  }
+};
+
+requestNotificationPermission();
+setupFirebaseNotifications();
 initShake();
-setupPushNotifications();
 
 AppRegistry.registerComponent(appName, () => App);
