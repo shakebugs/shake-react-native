4a72d2945ad7839d5fec74a647f2df81bfcf16eb
Mislav Stanić <mislav.stanic@hotmail.com>
2025-05-14 16:45:43 +0200

    SHK-7710: Enabled react-native-fs and react-native-device-info

--- example/src/ui/core/Version.tsx
+++ example/src/ui/core/Version.tsx
@@ -1,12 +1,11 @@
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
-// import DeviceInfo from 'react-native-device-info';
+import DeviceInfo from 'react-native-device-info';
 
 const Version = () => {
   return (
     <View style={styles.info}>
       <TouchableWithoutFeedback>
-        {/*<Text style={styles.version}>{'v' + DeviceInfo.getVersion()}</Text>*/}
-          <Text style={styles.version}>{'v1.0.0'}</Text>
+        <Text style={styles.version}>{'v' + DeviceInfo.getVersion()}</Text>
       </TouchableWithoutFeedback>
     </View>
   );
